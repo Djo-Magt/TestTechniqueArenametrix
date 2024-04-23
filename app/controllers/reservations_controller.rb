8cd2bd267bf11921db4ede0103ad16e46afc8178
Magnet Jonathan <j.magnet@outlook.com>
2024-04-23 13:56:33 +0200

    créa analyse

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,51 @@
+require 'csv'
+
 class ReservationsController < ApplicationController
   def import
-    return redirect_to reservations_path, notice: "Seulement CSV" unless params[:file].content_type == "text/csv"
+    file = params[:file]
+    if file.nil?
+      return redirect_to reservations_path, notice: "Pas de fichier"
+    else
+      return redirect_to reservations_path, notice: "Seulement CSV" unless file.content_type == "text/csv"
+    end
+    filename = file.original_filename
 
-    redirect_to reservations_path, notice: "fichier importé"
+    Reservation.destroy_all
+
+    csv_data = file.read
+    csv = CSV.parse(csv_data, headers: :first_row, col_sep: ';')
+    csv.each do |row|
+      reservation_hash = {}
+      reservation_hash[:numero_billet] = row["Numero billet"]
+      reservation_hash[:numero_reservation] = row["Reservation"]
+      reservation_hash[:date_reservation] = row["Date reservation"]
+      reservation_hash[:heure_reservation] = row["Heure reservation"]
+      reservation_hash[:cle_spectacle] = row["Cle spectacle"]
+      reservation_hash[:spectacle] = row["Spectacle"]
+      reservation_hash[:cle_representation] = row["Cle representation"]
+      reservation_hash[:representation] = row["Representation"]
+      reservation_hash[:date_representation] = row["Date representation"]
+      reservation_hash[:heure_representation] = row["Heure representation"]
+      reservation_hash[:date_fin] = row["Date fin representation"]
+      reservation_hash[:heure_fin] = row["Heure fin representation"]
+      reservation_hash[:prix] = row["Prix"]
+      reservation_hash[:type_de_produit] = row["Type de produit"]
+      reservation_hash[:filiere] = row["Filiere de vente"]
+      reservation_hash[:nom] = row["Nom"]
+      reservation_hash[:prenom] = row["Prenom"]
+      reservation_hash[:email] = row["Email"]
+      reservation_hash[:adresse] = row["Adresse"]
+      reservation_hash[:code_postal] = row["Code postal"]
+      reservation_hash[:pays] = row["Pays"]
+      reservation_hash[:age] = row["Age"]
+      reservation_hash[:sexe] = row["Sexe"]
+      Reservation.create(reservation_hash)
+    end
+
+    redirect_to reservations_path, notice: "Fichier '#{filename}' importé"
   end
 
-  def index
+  def analyse
     @reservations = Reservation.all
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # config/routes.rb
 
+  get "analyse", to: "reservations#analyse"
+
   resources :reservations do
     collection do
       post :import
